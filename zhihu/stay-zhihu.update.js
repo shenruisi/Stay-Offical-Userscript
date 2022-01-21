1361e358e1206660276d32072c6d9a2ec073e934
shen yin <tigris.shin@gmail.com>
2022-01-21 12:28:14 +0800

    file: zhihu

---
+++ zhihu/stay-zhihu.update.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         知乎@AutoExpand
+// @namespace    http://stay.app/
+// @version      0.0.2
+// @description  自动展开回答，支持知乎桌面版
+// @author       Stay²
+// @match        *://*.zhihu.com/*
+// @exclude      (http|https)://*.zhihu.com/R_E_D_I_R_E_C_T/*
+// @run-at       document-start
+// @grant        GM_log
+// @require      stay://vendor/stay-taskloop.js
+// @updateURL    
+// @downloadURL    
+// ==/UserScript==
