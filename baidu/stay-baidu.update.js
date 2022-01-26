a91dc77f95cfa8bd79491a38256e5c84b28b7d80
shen yin <tigris.shin@gmail.com>
2022-01-26 15:59:26 +0800

    feat baidu,google,toutiao

---
+++ baidu/stay-baidu.update.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         百度@PreventAppJump
+// @namespace    http://stay.app/
+// @version      0.0.2
+// @description  防止从Baidu结果页跳转到其他App
+// @author       Stay²
+// @match        *://*.baidu.com/*
+// @match        *://*.zhihu.com/R_E_D_I_R_E_C_T/*
+// @match        *://*.xiaohongshu.com/R_E_D_I_R_E_C_T/*
+// @exclude      (http|https)://tieba.baidu.com/*
+// @run-at       document-start
+// @require      stay://vendor/stay-taskloop.js
+// @updateURL    https://raw.githubusercontent.com/shenruisi/Stay-Offical-Userscript/main/baidu/stay-baidu.update.js
+// @downloadURL    https://raw.githubusercontent.com/shenruisi/Stay-Offical-Userscript/main/baidu/stay-baidu.user.js
+// ==/UserScript==
