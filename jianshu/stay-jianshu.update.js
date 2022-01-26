98a4fc560780ddc58c3d105d0484c9ae7abd6ade
shen yin <tigris.shin@gmail.com>
2022-01-26 15:49:59 +0800

    feat: jianshu

---
+++ jianshu/stay-jianshu.update.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         简书@AutoExpand
+// @namespace    http://stay.app/
+// @version      0.0.2
+// @description  自动展开简书文章
+// @author       Stay²
+// @match        *://*.jianshu.com/*
+// @run-at       document-start
+// @require      stay://vendor/stay-taskloop.js
+// ==/UserScript==
+
