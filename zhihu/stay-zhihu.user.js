5350ddbf16fa113b3fff7cf041133f07443a3da2
shen yin <tigris.shin@gmail.com>
2022-01-26 15:18:25 +0800

    feat: zhihu.js

--- zhihu/stay-zhihu.user.js
+++ zhihu/stay-zhihu.user.js
@@ -13,79 +13,127 @@
 // @downloadURL    https://raw.githubusercontent.com/shenruisi/Stay-Offical-Userscript/main/zhihu/stay-zhihu.user.js
 // ==/UserScript==
  
-function invirant(condition, message) {
-    if (!condition) {
-      throw '[豆瓣电影在线观看]', '获取电影名失败';
-    }
-  }
-  
-  function getMovieName() {
-    var innerText = document.querySelector('.related-info').innerText;
-    var movieName = innerText.substring(innerText, innerText.indexOf('的剧情简介'));
-    invirant(movieName);
-  
-    return movieName;
-  }
-  
-  function getMovieList(movieName) {
-    const url = 'https://api.jackeriss.com/api/v1/search/?text=' + movieName + '&type=0&from=0&size=10';
-    return fetch(url)
-      .then(function(res) {
-        return res.json();
-      })
-      .then(function({resources}) {
-        appendMovieLink(resources);
-      })
-  }
-  
-  function ensureContainer() {
-    var movieContainer = document.querySelector('.gray_ad');
-  
-    if (movieContainer && movieContainer.innerText.includes('在哪儿看这部')) {
-      return
-    }
-    var aside = document.querySelector('.aside');
-    var grayad = document.createElement('div');
-    grayad.className = 'gray_ad';
-  
-    grayad.innerHTML = [
-      '<h2>',
-        '在哪儿看这部电影 &nbsp;·&nbsp;·&nbsp;·&nbsp;·&nbsp;·&nbsp;·',
-      '</h2>',
-      '<ul class="bs">',
-      '</ul>'
-    ].join('');
-    aside.prepend(grayad);
-  }
-  
-  function appendMovieLink(resources) {
-    var internalPlatform = ['爱奇艺', '腾讯视频', '优酷', 'bilibili'];
-    var movieContainer = document.querySelector('.gray_ad');
-  
-    function append(resource) {
-      if (internalPlatform.includes(resource.website)) {
-        return;
+function removeChoosePanel(){
+  let pannels = document.querySelectorAll('.ModalWrap');
+  for (var i = 0; i < pannels.length; i++){
+    let pannel = pannels[i];
+    let div = pannel.querySelector('.ModalExp-modalShow');
+    if (div){
+      let btns = div.querySelectorAll('.ModalWrap-itemBtn');
+      
+      for (var i=0; i < btns.length; i++){
+        let button = btns[i];
+        if (button.innerText == "继续"){
+          button.click();
+          return COMPLETE;
+        }
       }
-  
-      var ul = movieContainer.querySelector('ul');
-      var htmlContent = [
-        '<a class="playBtn" target="_blank" href=', resource.url, '>',
-        resource.website,
-        '</a>',
-        '<span class="buylink-price" style="left: 130px; position: absolute; color: #999;"><span>免费观看</span></span>'
-      ].join('');
-      var li = document.createElement('li');
-      li.innerHTML = htmlContent;
-      ul.append(li);
     }
-  
-    resources.forEach(append);
   }
   
-  (function() {
-    'use strict';
+  return CONTINUE;
+}
+
+function removeAppJump(){
+  let app = document.querySelector('button.OpenInAppButton');
+  if (app){
+    var url = app.getAttribute("urlscheme");
+    app.remove();
+    return DATA();
+  }
+  return CONTINUE;
+}
+
+function unfold(){
+  let btns = document.querySelectorAll('.ContentItem-expandButton');
+  for (var i = 0; i < btns.length; i++){
+    let btn = btns[i];
+    if (/展开阅读全文/.test(btn.innerText)){
+      let content = btn.parentNode;
+      if (content){
+        content.className = "RichContent RichContent--unescapable";
+        let contentinner = content.querySelector('.RichContent-inner');
+        if (contentinner){
+          contentinner.style.maxHeight = "none";
+        }
+      }
+      btn.remove();
+      let main = document.querySelector(".Question-main");
+      if (main){
+        main.onclick = function(event){
+          let target = event.target;
+          while(!/RichContent/.test(target.className) && target.tagName != "BODY"){
+            target = target.parentNode;
+          }
+          if (/RichContent/.test(target.className)){
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          
+        }
+      }
+      
+      return COMPLETE;
+    }
+  }
   
-    ensureContainer();
-    var movieName = getMovieName();
-    getMovieList(movieName);
-  })();
+  return CONTINUE;
+}
+
+function replaceDirectUrl(){
+  let items = document.querySelectorAll('.Feed');
+  if (items){
+    for (var i = 0; i < items.length; i++){
+      let item = items[i];
+      let a = item.querySelector('a');
+      item.onclick = function(event){
+        event.preventDefault();
+        event.stopPropagation();
+        location.href = a.href;
+      }
+    }
+    return COMPLETE;
+  }
+  
+  return CONTINUE_TIL(10);
+}
+
+function supportDesktopBrowsingIfNeeded(){
+  console.log("supportDesktopBrowsing",window.navigator.userAgent);
+  if (/Macintosh/.test(window.navigator.userAgent)){
+    var css = "*{box-sizing:border-box;min-width:initial!important;max-width:100%!important;}html{overflow-y:auto!important;}body{position:absolute!important;top:0!important;width:100%;-webkit-font-smoothing:antialiased;text-rendering:optimizeLegibility;}.RichContent-actions.is-fixed,.Question-sideColumn,.ContentLayout-sideColumn,.QuestionHeader-footer,#free-reward-panel,.show-foot,.meta-bottom{display:none!important;}.Question-mainColumn,.ContentLayout-mainColumn{width:100%!important;margin:0!important;padding:6px!important;}#free-reward-panel,.AdblockBanner,.AppHeader-Tabs,.AppHeader-userInfo,.Modal-wrapper,.Pc-word,.Question-sideColumn,.QuestionHeader-footer,.RichContent-actions.is-fixed,.meta-bottom,.show-foot,body>div:last-child,.Question-mainColumnLogin,.Profile-sideColumn,.QuestionHeader-side,.ModalWrap,.ShareMenu{display:none!important;}.Topstory{padding:0!important;}.GlobalSideBar,.Question-sideColumn,.QuestionHeader-side,.TopstoryItem--advertCard,.TopstorySideBar,.css-1qefhqu{display:none!important;}.AppHeader,.AppHeader-widescreenResponsive.AppHeader-inner{box-sizing:content-box!important;width:100%!important;min-width:100%!important;padding:0!important;}.QuestionHeader{min-width:initial;}.Question-mainColumn,.Topstory-mainColumn,.TopstoryMain,.TopstoryV2-mainColumn{width:100%!important;max-width:100%!important;}.Topstory-mainColumn{margin:0!important;}.TopstoryItem.ZVideoItem{margin:0;}.TopstoryItem.ZVideoItem.RichContent{overflow:hidden;margin-top:20px;}.ContentItem-more.ContentItem-arrowIcon{vertical-align:-2px;}.AnswerItem,.ArticleItem,.Layout-main.av-card{font-size:120%!important;}.RichTextpre{font-size:90%!important;padding:0.8em1.2em!important;padding-top:0.6em;padding-bottom:0.6em;}.RichTextcode,.RichTextpre{font-family:\"JetBrainsMono\",FiraMono-Regular,Menlo,Courier,monospace!important;}.ContentItem-time,.Post-Header,.Post-NormalMain>div,.Post-NormalSub.Comments-container,.Post-RichTextContainer,.Post-topicsAndReviewer,.PostIndex-Contributions,.TitleImage{width:100%!important;}.Post-RichText{font-size:18px;}.Footer{display:none!important;}.SearchResult-Card.ContentItem-title{color:#0084ff!important;}.Search-container.RichContent.is-collapsed.RichContent-inner{font-size:16px!important;}.Profile-lightList{padding:6px20px;}.Profile-lightItem:first-child{border-top:0none!important;}.Profile-lightItem:last-child{border-bottom:0none!important;}.ProfileMain.RichContent{line-height:1.8;}.ContentItem-actions{margin:6px0;padding:0;flex-wrap:wrap;justify-content:space-between;}.QuestionHeader-main,.Question-main{padding:0;}.Post-Main{margin:02em;}.AppHeader-inner.css-qqgmyv{padding:6px10px;}.QuestionHeader-content{padding:012px;}.List-item{padding:10px12px;}";
+    var node = document.createElement("style");
+    node.type = "text/css";
+    node.appendChild(document.createTextNode(css));
+    var heads = document.getElementsByTagName("head");
+    if (heads.length > 0) {
+      heads[0].appendChild(node);
+    } else {
+      document.documentElement.appendChild(node);
+    }
+  }
+  else{
+    let tasks = [removeChoosePanel,removeAppJump,replaceDirectUrl,unfold];
+    Stay_Inject.run(tasks,100,30,false).then((data) => {});
+  }
+}
+
+if (document.readyState !== "loading"){
+  supportDesktopBrowsingIfNeeded();
+}
+else{
+  document.addEventListener("DOMContentLoaded", function(event) {
+    supportDesktopBrowsingIfNeeded();
+  });
+}
+
+document.addEventListener("scroll", function(event) {
+  if (!/Macintosh/.test(window.navigator.userAgent)){
+    let guide = document.querySelector('div.DownloadGuide');
+    if (guide){
+      console.log(guide);
+      guide.style.display = "none";
+    }
+  }
+  
+});
