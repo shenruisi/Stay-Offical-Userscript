85d34db32dabc851590314bec7308a16a07cd4ec
shen yin <tigris.shin@gmail.com>
2022-05-20 17:43:44 +0800

    Update stay-baidu.update.js

--- baidu/stay-baidu.update.js
+++ baidu/stay-baidu.update.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         百度@PreventAppJump
 // @namespace    http://stay.app/
-// @version      0.0.4
+// @version      0.0.5
 // @description  防止从Baidu结果页跳转到其他App
 // @author       Stay²
 // @match        *://*.baidu.com/*
